86ba65224639c22d1c7f275d9b32871d14f2bce7
Kei Kurono <keikurono01@gmail.com>
2014-04-24 16:52:20 -0300

    Toolbox Fast Marching for GNU/Linux

---
+++ geodesic_trace_back.m
@@ -0,0 +1,18 @@
+function path = geodesic_trace_back(algorithm, destination)
+
+global geodesic_library;
+
+tmp{1} = destination;
+d = geodesic_convert_surface_points(tmp);
+
+tmp = libpointer('doublePtrPtr');
+[path_length, tmp, path_double] = calllib(geodesic_library, 'trace_back', algorithm.id, d, tmp);
+
+if (~path_length),
+    path = [];
+    return;
+end
+
+setdatatype(path_double, 'doublePtr', path_length*5);
+
+path = geodesic_convert_surface_points(path_double.Value);
